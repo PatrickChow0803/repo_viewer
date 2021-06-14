99745864d350d817fdb48eeda3855b3c498a1651
patrickchow0803 <patrickchow0803@gmail.com>
2021-06-14 11:34:22 -0400

    added Authorization Code Grant

--- lib/auth/infrastructure/github_authenticator.dart
+++ lib/auth/infrastructure/github_authenticator.dart
@@ -59,4 +59,17 @@ class GithubAuthenticator {
   // which therefore returns true
   Future<bool> isSignedIn() =>
       getSignedInCredentials().then((credentials) => credentials != null);
+
+  AuthorizationCodeGrant createGrant() {
+    return AuthorizationCodeGrant(
+      clientId,
+      authorizationEndpoint,
+      tokenEndpoint,
+      secret: clientSecret,
+    );
+  }
+
+  Uri getAuthorizationUrl(AuthorizationCodeGrant grant) {
+    return grant.getAuthorizationUrl(redirectUrl, scopes: scopes);
+  }
 }
