abab37f9ec7b2609fccf8069d1081ef5304587df
patrickchow0803 <patrickchow0803@gmail.com>
2021-06-14 20:51:14 -0400

    added AuthNotifier

--- lib/auth/application/auth_notifier.dart
+++ lib/auth/application/auth_notifier.dart
@@ -1,6 +1,12 @@
 import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:repo_viewer/auth/infrastructure/github_authenticator.dart';
 
 part 'auth_notifier.freezed.dart';
+
+// states come out from the application layer and they're useful in the presentation layer
+// application layer is responsible for transforming the infrastructure data into something
+// the presentation layer can understand
 
 @freezed
 class AuthState with _$AuthState {
@@ -11,4 +17,17 @@ class AuthState with _$AuthState {
   const factory AuthState.unauthenticated() = _Unauthenticated;
   const factory AuthState.authenticated() = _Authenticated;
   const factory AuthState.failure() = _Failure;
+}
+
+class AuthNotifier extends StateNotifier<AuthState> {
+  final GithubAuthenticator _authenticator;
+  AuthNotifier(this._authenticator) : super(const AuthState.initial());
+
+  // transform from the infrastructure layer into something understandable in the application layer
+  Future<void> checkAndUpdateAuthState() async {
+    // if the user is signed in, make the state authenticated else opposite
+    state = (await _authenticator.isSignedIn())
+        ? const AuthState.authenticated()
+        : const AuthState.unauthenticated();
+  }
 }
