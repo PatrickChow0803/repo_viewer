498ddea101d7d4f7a78142403245614be003524a
patrickchow0803 <patrickchow0803@gmail.com>
2021-06-29 14:35:54 -0400

    finished auth_application layer

--- lib/auth/application/auth_notifier.dart
+++ lib/auth/application/auth_notifier.dart
@@ -9,11 +9,15 @@ part 'auth_notifier.freezed.dart';
 // application layer is responsible for transforming the infrastructure data into something
 // the presentation layer can understand
 
+// in the case of having something async and can change in time
+// and has multiple distent cases, should use a freezed union
+
 @freezed
 class AuthState with _$AuthState {
   const AuthState._();
   // initial state since don't know if the user is authenticated or not on start up
   // getSignedInCredentials is being called during this
+  // this is what tells the presentation layer to display the loading screen
   const factory AuthState.initial() = _Initial;
   const factory AuthState.unauthenticated() = _Unauthenticated;
   const factory AuthState.authenticated() = _Authenticated;
@@ -23,11 +27,12 @@ class AuthState with _$AuthState {
 // Uri is the redirect url
 typedef AuthUriCallback = Future<Uri> Function(Uri authorizationUrl);
 
+// logic for the notifier
 class AuthNotifier extends StateNotifier<AuthState> {
   final GithubAuthenticator _authenticator;
   AuthNotifier(this._authenticator) : super(const AuthState.initial());
 
-  // transform from the infrastructure layer into something understandable in the application layer
+  // transform from the infrastructure layer into something understandable in the presentation layer
   Future<void> checkAndUpdateAuthState() async {
     // if the user is signed in, make the state authenticated else opposite
     state = (await _authenticator.isSignedIn())
@@ -35,6 +40,7 @@ class AuthNotifier extends StateNotifier<AuthState> {
         : const AuthState.unauthenticated();
   }
 
+  // used to get the signin webpage to the presentation layer
   Future<void> signIn(AuthUriCallback authorizeCallback) async {
     final grant = _authenticator.createGrant();
     final redirectUrl =
